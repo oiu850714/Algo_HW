83f643e9f6be0ea3369ab24a973374f88f1ca25d
oiu850714 <oiu850714@gmail.com>
2017-11-14 17:14:06 +0800

    complete 2-0-Kind.cpp

--- HW2/2-0-Kind.cpp
+++ HW2/2-0-Kind.cpp
@@ -7,6 +7,8 @@ using std::vector;
 using std::max;
 using std::lower_bound;
 
+//IN THIS SHITCODE, KEEP ALL FUCKING OFFSETS IN MIND, SOME OF LOOPS MAY RUTN N+1 TIMES
+
 void get_answer(int n, vector<int> &answer_arr)
 {
     answer_arr.push_back(0);// FUCK YOU OFFSET
@@ -21,7 +23,7 @@ void get_answer(int n, vector<int> &answer_arr)
 int LIS(vector<int> &seq, const int n)
 {
     vector<int> IS_of_length_i(n + 1, INT32_MAX);
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i <= n; i++)
     {
         auto location = lower_bound(IS_of_length_i.begin(), IS_of_length_i.end(), seq[i]);
         *location = seq[i];
@@ -37,13 +39,13 @@ int LIS(vector<int> &seq, const int n)
 int LCS(vector<int> &first_seq, vector<int> &sec_seq, const int n)
 {
     vector<int> indices_of_sec_seq_i_in_first_seq(n + 1, 0);
-    // define indices_of_sec_seq_i_in_first_seq[i]: the index of sec_seq[i],
-    // which is a charactor appearing in first_seq, in first_seq
 
     //create hash table
     for(int i = 0; i <= n; i++)
     {
         indices_of_sec_seq_i_in_first_seq[first_seq[i]] = i;
+        // let first_seq[i]'s code be i
+        // then indices_of_sec_seq_i_in_first_seq[first_seq[i]] is i
         //e.g.
         // 1  2  3  4  5  6  7  8  9
         //[a][d][e][c][b][f][g][h][i]
@@ -57,6 +59,7 @@ int LCS(vector<int> &first_seq, vector<int> &sec_seq, const int n)
     {
         sec_seq_to_encode_array[i] = indices_of_sec_seq_i_in_first_seq[sec_seq[i]];
     }
+    //encode sec_seq
 
     return LIS(sec_seq_to_encode_array, n);
 }
